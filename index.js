5341f5c7ee9eb832243229716e2ef49765405fb0
Jared <kuil5230@gmail.com>
2018-09-09 02:39:22 +0800

    .

--- index.js
+++ index.js
@@ -2,26 +2,40 @@ import cheerio from "cheerio";
 import fs from "fs";
 import axios from "axios";
 
-const result = [];
-const linkList = [];
+const exportResults = (parsedResults) => {
+  fs.readFile('./question.json', 'utf-8', (err, data) => {
+    if (err) console.log('read', err)
+    const jsonData = JSON.parse(data);
+
+    parsedResults.map(item => {
+      jsonData.push(item);
+      fs.writeFile("./question.json", JSON.stringify(jsonData), (err) => {
+        if (err) console.log('write', err);
+
+      });
+    });
+  });
+}
 
 const getWebSiteContent = async url => {
+  const result = [];
+  const linkList = [];
   try {
     const ResponseHTML = await axios.get(url);
     const $ = cheerio.load(ResponseHTML.data);
     $(".subject > span > a").each((index, value) => {
-      linkList.push("https://www.mobile01.com/" + $(value).attr("href"));
+      linkList.push({ link: "https://www.mobile01.com/" + $(value).attr("href"), page: $('.numbers').text() });
     });
   } catch (error) {
     console.log(error);
   }
 
   linkList.map(link => {
-    axios.get(link).then(ResponseHTML => {
+    axios.get(link.link).then(ResponseHTML => {
       const $ = cheerio.load(ResponseHTML.data);
       //迭代所有回覆
       const ReplyList = [];
-      $(".single-post-content").each((index, value) => {
+      $(".single-post-content").map((index, value) => {
         ReplyList.push(
           $(value)
             .text()
@@ -29,36 +43,29 @@ const getWebSiteContent = async url => {
         );
       });
 
-      result.push(
-        Object.assign({
-          title: $("#forum-content-main")
-            .find("h1")
-            .text(),
-          content: $(".single-post-content")
-            .first()
-            .text()
-            .replace(new RegExp("\\n|\\s+", "g"), ""),
-          reply: ReplyList
-        })
-      );
-      console.log(
-        $("#forum-content-main")
+      result.push({
+        title: $("#forum-content-main")
           .find("h1")
+          .text(),
+        content: $(".single-post-content")
+          .first()
           .text()
-      );
-      fs.writeFileSync("./question.json", JSON.stringify(result), err => {
-        if (err) throw err;
+          .replace(new RegExp("\\n|\\s+", "g"), ""),
+        reply: ReplyList,
+        page: link.page
       });
+      exportResults(result);
+    }).catch(error => {
+      console.log(error);
     });
   });
 };
 
-// getWebSiteContent("https://www.mobile01.com/topiclist.php?f=291&p=");
-
-for (let i = 0; i < 10; i++) {
+for (let i = 7; i < 2900; i++) {
   (num => {
     setTimeout(() => {
       getWebSiteContent("https://www.mobile01.com/topiclist.php?f=291&p=" + num);
-    }, 1000 * (i + 1));
+      console.log(num)
+    }, 5000 * (i + 1));
   })(i);
 }
