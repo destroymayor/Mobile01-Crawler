d511f1e8ac5d3ae9a7ad1c43cf6ef051ce360f9d
Jared <kuil5230@gmail.com>
2018-09-09 23:01:59 +0800

    .

--- index.js
+++ index.js
@@ -2,70 +2,92 @@ import cheerio from "cheerio";
 import fs from "fs";
 import axios from "axios";
 
-const exportResults = (parsedResults) => {
-  fs.readFile('./question.json', 'utf-8', (err, data) => {
-    if (err) console.log('read', err)
-    const jsonData = JSON.parse(data);
-
-    parsedResults.map(item => {
-      jsonData.push(item);
-      fs.writeFile("./question.json", JSON.stringify(jsonData), (err) => {
-        if (err) console.log('write', err);
-
-      });
+const readFileAsync = path => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, (err, data) => {
+      if (err) reject(err);
+      resolve(data);
     });
   });
-}
+};
+
+const exportResults = parsedResults => {
+  readFileAsync("./question.json")
+    .then(data => {
+      const jsonData = JSON.parse(data);
+      jsonData.push(parsedResults);
+
+      fs.writeFile("./question.json", JSON.stringify(jsonData), err => {
+        if (err) console.log("write", err);
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
 
 const getWebSiteContent = async url => {
-  const result = [];
   const linkList = [];
   try {
     const ResponseHTML = await axios.get(url);
     const $ = cheerio.load(ResponseHTML.data);
     $(".subject > span > a").each((index, value) => {
-      linkList.push({ link: "https://www.mobile01.com/" + $(value).attr("href"), page: $('.numbers').text() });
+      linkList.push({ link: "https://www.mobile01.com/" + $(value).attr("href"), page: $(".numbers").text() });
     });
   } catch (error) {
     console.log(error);
   }
 
-  linkList.map(link => {
-    axios.get(link.link).then(ResponseHTML => {
+  const result = [];
+  const LinkAsyncRequest = async (url, page) => {
+    try {
+      const ResponseHTML = await axios.get(url);
       const $ = cheerio.load(ResponseHTML.data);
-      //迭代所有回覆
+
       const ReplyList = [];
       $(".single-post-content").map((index, value) => {
         ReplyList.push(
-          $(value)
+          $(".single-post-content")
+            .eq(index + 1)
             .text()
-            .replace(new RegExp("\\n|\\s+", "g"), "")
+            .replace(new RegExp("\\n|\\s+|{|}|\"|'", "g"), "")
         );
       });
 
       result.push({
-        title: $("#forum-content-main")
-          .find("h1")
+        title: $("#forum-content-main > h1")
+          .first()
           .text(),
         content: $(".single-post-content")
           .first()
           .text()
-          .replace(new RegExp("\\n|\\s+", "g"), ""),
+          .replace(new RegExp("\\n|\\s+|{|}|\"|'", "g"), ""),
         reply: ReplyList,
-        page: link.page
+        page: page
       });
-      exportResults(result);
-    }).catch(error => {
-      console.log(error);
-    });
-  });
+
+      console.log(page);
+    } catch (error) {}
+
+    await exportResults(result);
+  };
+
+  Promise.all(
+    linkList.map(async link => {
+      await LinkAsyncRequest(link.link, link.page);
+    })
+  );
 };
 
-for (let i = 7; i < 2900; i++) {
-  (num => {
-    setTimeout(() => {
-      getWebSiteContent("https://www.mobile01.com/topiclist.php?f=291&p=" + num);
-      console.log(num)
-    }, 5000 * (i + 1));
-  })(i);
-}
+const runFunction = (pageNumber, total) => {
+  for (let i = 1; i < total; i++) {
+    (num => {
+      setTimeout(() => {
+        getWebSiteContent("https://www.mobile01.com/topiclist.php?f=291&p=" + (num + pageNumber));
+        console.log(num + pageNumber);
+      }, 2000 * (i + 1));
+    })(i);
+  }
+};
+
+runFunction(350, 2900);
