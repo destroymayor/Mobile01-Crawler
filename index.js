121d8e4ad0f993efdcfb127ab69971ec94dfc962
Jared <kuil5230@gmail.com>
2018-11-02 22:06:35 +0800

    .

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import { startCrawler, fileMerger } from './crawler'
+import { startCrawler, fileMerger } from "./crawler";
 
 // forum = 討論區代號
 // startCode = 從第幾頁開始爬
 // total = 該討論區總共頁數
 
-//startCrawler(174, 1, 1701);
+startCrawler(174, 1, 1701);
 
 //mainFile, mergerFile
 //fileMerger('./data/computer/computer.json', './data/computer/605.json');
